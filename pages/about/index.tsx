85e2a5e68645f0667b20ff515040668e42dd39a8
Yan <ssffggdd@mail.ru>
2022-03-10 14:06:59 +0300

    scroll

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Image from "next/image";
 import { useRouter } from 'next/router'
 
-export default function index() {
+export default function Index() {
 
 
     const [able, setAble] = useState(true)
